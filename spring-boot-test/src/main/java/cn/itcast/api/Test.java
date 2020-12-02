e809864e4f7ae53e81004748a3b9b700a4e9a424
admin <1101403286@qq.com>
2020-12-02 22:24:26 +0800

    新增更新Test测试提交到github------------再一次修改

--- spring-boot-test/src/main/java/cn/itcast/api/Test.java
+++ spring-boot-test/src/main/java/cn/itcast/api/Test.java
@@ -3,5 +3,6 @@ package cn.itcast.api;
 public class Test {
     public static void main(String[] args) {
         System.out.println("github克隆工程");
+        System.out.println("github克隆工程");
     }
 }
